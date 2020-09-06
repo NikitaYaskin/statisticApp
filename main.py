8cae01abd01279a9ad49eb6cc145aba010e88f46
Nikita <nikuuu66@gmail.com>
2020-09-06 23:11:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ def connect():
     a = {}
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
-    for row in c.execute('SELECT identificationcode, phonenumber, name, dateofapplicationcompletion, dateandtimeofreceipt, department, product FROM data'):
+    for row in c.execute('SELECT identificationcode, phonenumber, name, dateofapplicationcompletion, dateandtimeofreceipt, department, product FROM data WHERE haveused = 0'):
         a[str(row[0])] = row
     return a
     conn.close()
 
 def createTable(mess):
     for k, v in mess.items():
-        row = '<tr><td>' + str('</td><td>'.join(map(str, v))) + '</td><input type="checkbox"></tr>'
+        row = '<tr><td>' + str('</td><td>'.join(map(str, v))) + '''</td><td><select name="status">
+                                                                      <option value=0>Ще не скористався</option>
+                                                                      <option value=1>Скористався</option>
+                                                                      <option value=2>Відмовився</option>
+                                                                    </select></td></tr>'''
         eel.addTable(row)
     
 
